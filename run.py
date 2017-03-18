27c40d8a0c9807b1cddae1cfc0c67436dfd63da4
Jeremy Tarling <jeremy.tarling@gmail.com>
2017-03-18 11:35:31 +0000

    added web wrapper (flask)

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-# Imports the app variable from our app package 
-# and invokes its run method to start the server. 
-# (the app variable holds the Flask instance)
+# start the server - 
+# imports the app variable from the webapp subdirectory (package) 
+# and invokes its run method to start the server (app is an instance of Flask)
 
 from webapp import app
 app.run(debug=True)
