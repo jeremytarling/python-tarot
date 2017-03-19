332eeacbac3523957dc6eb6fc1d3b45422342cdf
Jeremy Tarling <jeremy.tarling@gmail.com>
2017-03-19 17:51:37 +0000

    readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # python-tarot
-simple tarot card reader in python
+
+A simple tarot card reader in Python.
+
+Card images used are out of copyright.
+
+Deployed to Heroku here: https://python-tarot.herokuapp.com/
+
+Please feel free to fork or re-use, no license restrictions (this was my teach-yourself-Python-webapps project).
+
+
--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask
 app = Flask(__name__)
 
-# and import the views module
+# and import the routes
+# (routes is a file in the webapp directory)
 from webapp import routes
-# import comes at the end (because views needs app)
+# this import comes at the end because routes needs to know abut the app object's methods
