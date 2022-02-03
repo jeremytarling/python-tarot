31fa67fa2222c0b86af42df4922a7145e2453a2d
Jeremy Tarling <jeremy.tarling@gmail.com>
2022-02-03 17:11:16 +0000

    fixed out of range error on dic, added missing 10 of cups

--- webapp/cards.py
+++ webapp/cards.py
@@ -6,7 +6,7 @@ def shuffle_deck(deck) :
 
 # pick card(s) - call this method multiple times to draw multiple unique cards from the deck
 def get_cards(deck) :
-	num = random.randint(0, len(deck))
+	num = random.randint(0, len(deck) -1)
 	card = deck[num]
 	del(deck[num]) # so we don't get the same card twice if we're calling this multiple times for the same hand
 	rev = random.randint(-1,1) # is card reversed? zero (false) or 1 (true)
