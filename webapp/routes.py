d8a20358f19694af37993e9259438d9c5434f214
theredwillow <theredwillows@gmail.com>
2024-08-21 21:44:44 -0700

    Fixed mis-casing (cardtype -> card_type)

--- webapp/routes.py
+++ webapp/routes.py
@@ -29,7 +29,7 @@ def one_card():
 	my_deck = deck.get_deck()
 	my_card = deck.get_card(my_deck)
 
-	if my_card[0]['cardtype'] == "major" :
+	if my_card[0]['card_type'] == "major" :
 		return render_template("one_card.html",
 								name = my_card[0]['name'],
 								title = my_card[0]['name'],
@@ -39,7 +39,7 @@ def one_card():
 								reversed_meaning = my_card[0]['rdesc'],
 								image = my_card[0]['image'],
 								url = my_card[0]['url'],
-								cardtype = my_card[0]['cardtype'])
+								card_type = my_card[0]['card_type'])
 	else:
 		return render_template("one_card.html",
 								name = my_card[0]['name'],
@@ -49,7 +49,7 @@ def one_card():
 								reversed_meaning = my_card[0]['rdesc'],
 								image = my_card[0]['image'],
 								url = my_card[0]['url'],
-								cardtype = my_card[0]['cardtype'])
+								card_type = my_card[0]['card_type'])
 
 # get three cards
 @app.route('/three-cards', strict_slashes=False)
@@ -77,7 +77,7 @@ def specific_card(card_url):
 		next_card_url = '/one-card/' + list(filter(lambda next_card: next_card['sequence'] == (my_card['sequence'] +1), my_deck))[0]['url']
 	else :
 		next_card_url = '/tarot-study'
-	if my_card['cardtype'] == "major" :
+	if my_card['card_type'] == "major" :
 		return render_template("specific_card.html",
 								name = my_card['name'],
 								title = my_card['name'],
@@ -91,7 +91,7 @@ def specific_card(card_url):
 							    previous = previous_card_url,
 							    next = next_card_url,
 							    sequence = my_card['sequence'],
-							    cardtype = my_card['cardtype'])
+							    card_type = my_card['card_type'])
 	else: 
 		return render_template("specific_card.html",
 								name = my_card['name'],
@@ -103,6 +103,6 @@ def specific_card(card_url):
 							    previous = previous_card_url,
 							    next = next_card_url,
 							    sequence = my_card['sequence'],
-							    cardtype = my_card['cardtype'])
+							    card_type = my_card['card_type'])
 
 
