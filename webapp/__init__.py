e4cd8d483ae6651e67ef333487bf9016be549b93
theredwillow <theredwillows@gmail.com>
2024-08-21 21:47:45 -0700

    Add linting ignores

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,8 @@
 # create a Flask object
-from flask import Flask
+from flask import Flask # type: ignore
 app = Flask(__name__)
 
 # and import the routes
 # (routes is a file in the webapp directory)
-from webapp import routes
+from webapp import routes  # noqa: E402, F401
 # this import comes at the end because routes needs to know abut the app object's methods
